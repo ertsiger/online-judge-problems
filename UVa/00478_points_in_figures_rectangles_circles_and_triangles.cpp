6367588b94841140c1168f5c3d9f08845cefc3ce
DanielFB <danielfb93@gmail.com>
2016-05-29 19:27:42 +0200

    problem 478 done

---
+++ UVa/00478_points_in_figures_rectangles_circles_and_triangles.cpp
@@ -0,0 +1,175 @@
+/**
+ * Author: Ertsiger
+ * 29/05/2016
+ * Time: 0.020
+ */
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+double INF = 1e100;
+double EPS = 1e-12;
+
+struct PT {
+    double x, y;
+    PT() {}
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &p)  const { return PT(x + p.x, y + p.y); }
+    PT operator - (const PT &p)  const { return PT(x - p.x, y - p.y); }
+    PT operator * (double c)     const { return PT(x*c, y*c); }
+    PT operator / (double c)     const { return PT(x / c, y / c); }
+};
+
+double dot(PT p, PT q)     { return p.x*q.x + p.y*q.y; }
+double dist2(PT p, PT q)   { return dot(p - q, p - q); }
+double cross(PT p, PT q)   { return p.x*q.y - p.y*q.x; }
+
+bool PointInPolygon(const vector<PT> &p, PT q) {
+    bool c = 0;
+    for (int i = 0; i < p.size(); i++){
+        int j = (i + 1) % p.size();
+        if ((p[i].y <= q.y && q.y < p[j].y ||
+            p[j].y <= q.y && q.y < p[i].y) &&
+            q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y))
+            c = !c;
+    }
+    return c;
+}
+
+struct Figure
+{
+    virtual bool containsPoint(double x, double y) = 0;
+};
+
+struct Rectangle : public Figure
+{
+    vector<PT> p;
+
+    Rectangle(double ulx, double uly, double lrx, double lry)
+    {
+        p.push_back(PT(ulx, uly));
+        p.push_back(PT(ulx, lry));
+        p.push_back(PT(lrx, lry));
+        p.push_back(PT(lrx, uly));
+    }
+
+    virtual bool containsPoint(double x, double y)
+    {
+        return PointInPolygon(p, PT(x, y));
+    }
+};
+
+struct Circle : public Figure
+{
+    double _radius;
+    PT center;
+
+    Circle(double cx, double cy, double radius)
+        : _radius(radius)
+    {
+        center.x = cx;
+        center.y = cy;
+    }
+
+    virtual bool containsPoint(double x, double y)
+    {
+        if (dist2(PT(x, y), center) <= _radius * _radius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+struct Triangle : public Figure
+{
+    vector<PT> p;
+
+    Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+    {
+        p.push_back(PT(x1, y1));
+        p.push_back(PT(x2, y2));
+        p.push_back(PT(x3, y3));
+    }
+
+    virtual bool containsPoint(double x, double y)
+    {
+        return PointInPolygon(p, PT(x, y));
+    }
+};
+
+int main()
+{
+    char type = ' ';
+
+    vector<Figure*> figures;
+
+    while (cin >> type)
+    {
+        if (type == '*')
+        {
+            break;
+        }
+        else if (type == 'r')
+        {
+            double ulx, uly, lrx, lry;
+            cin >> ulx >> uly >> lrx >> lry;
+
+            Figure* f = new Rectangle(ulx, uly, lrx, lry);
+            figures.push_back(f);
+        }
+        else if (type == 'c')
+        {
+            double cx, cy, r;
+            cin >> cx >> cy >> r;
+
+            Figure* f = new Circle(cx, cy, r);
+            figures.push_back(f);
+        }
+        else if (type == 't')
+        {
+            double x1, y1, x2, y2, x3, y3;
+            cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+            Figure* f = new Triangle(x1, y1, x2, y2, x3, y3);
+            figures.push_back(f);
+        }
+    }
+
+    double px, py;
+    int id = 1;
+    while (cin >> px >> py)
+    {
+        if (px == 9999.9 && py == 9999.9)
+        {
+            break;
+        }
+
+        bool iscontained = false;
+
+        for (int i = 0; i < figures.size(); ++i)
+        {
+            if (figures[i]->containsPoint(px, py))
+            {
+                cout << "Point " << id << " is contained in figure " << (i + 1) << endl;
+                iscontained = true;
+            }
+        }
+
+        if (!iscontained)
+        {
+            cout << "Point " << id << " is not contained in any figure" << endl;
+        }
+
+        ++id;
+    }
+
+    return 0;
+}
+
