42b36d01dace2730464d4cfcf8db26bea0e46666
DanielFB <danielfb93@gmail.com>
2016-11-03 14:30:39 +0100

    canvas painting problem solved (huffman codes)

---
+++ ACM-ICPC-Live-Archive/7274_canvas_painting.cpp
@@ -0,0 +1,52 @@
+/**
+ * Ertsiger
+ * 03/11/2016
+ * Time: 1.023
+ */
+#include <iostream>
+#include <set>
+
+#define LL long long
+
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+
+    int n = 0;
+    LL s = 0;
+
+    while (t--)
+    {
+        multiset<LL> canvas;
+
+        cin >> n;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> s;
+            canvas.insert(s);
+        }
+
+        LL totalInk = 0;
+
+        while (canvas.size() > 1)
+        {
+            LL a = *canvas.begin();
+            canvas.erase(canvas.begin());
+
+            LL b = *canvas.begin();
+            canvas.erase(canvas.begin());
+
+            totalInk += a + b;
+            canvas.insert(a + b);
+        }
+
+        cout << totalInk << endl;
+    }
+
+    return 0;
+}
+
