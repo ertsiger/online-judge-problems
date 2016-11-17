118dfde5209666b65a6efa3d4a242a4280334c32
Daniel Furelos Blanco <danielfb93@gmail.com>
2016-11-17 16:26:51 +0100

    modified fenwick tree

--- Notebook-2016/Data Structures/Binary Indexed Tree.cpp
+++ Notebook-2016/Data Structures/Binary Indexed Tree.cpp
@@ -1,15 +1,19 @@
 /* Binary indexed tree. Supports cumulative sum queries in O(log n) */
 #define N (1<<18)
-LL bit[N];
+#define LL long long 
 
-void add(LL* bit,int x,int val) {
-    for(; x<N; x+=x&-x)
-        bit[x]+=val;
+LL bit[N]={0};	//Binary Indexed Tree , nElements +1 positions
+int arr[N]={0};	//Array that represents the BIT (simple data, no cumulative) , nElements +1 positions
+//CAUTION !! INDEX STARTS IN 1
+void update(LL* bit, int* arr,int x,int val) {	//add or update a value
+    int dif = val - arr[x]; //diference between previous value and new value
+    arr[x] = val;           //set new value in the array
+    for(; x<N; x+=x&-x)     //jumps through indexes by jumps of the last 1 bit adding
+        bit[x]+=dif;        //uploads the tree values
 }
-
-LL query(LL* bit,int x) {
+LL query(LL* bit,int x) {   //acumula desde x hasta 0 
     LL res=0;
-    for(;x;x-=x&-x)
+    for(;x;x-=x&-x)         //salta quitando el bit de menor peso
         res+=bit[x];
     return res;
 }
